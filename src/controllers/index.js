acc46e905562103491d9cb3114b672c1d062ea32
liujinge <liujinge@tenwit.com>
2021-08-07 18:47:22 +0800

    feat(./): 新增 service 和 controller

---
+++ src/controllers/index.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const shopController = require('./shop');
+
+module.exports = async function initControllers() {
+  const router = Router();
+
+  router.use('/api/shop', await shopController());
+
+  return router;
+};
--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const { resolve } = require('path');
 const { promisify } = require('util');
+const initControllers = require('./controllers');
 
 const server = express();
 const port = parseInt(process.env.PORT || '9000');
 const publicDir = resolve('public');
 
 async function bootstrap() {
+  // 静态资源
   server.use(express.static(publicDir));
+
+  // 动态数据
+  server.use(await initControllers());
+
+  // 启动
   await promisify(server.listen.bind(server, port))();
   console.log(`===> Node started on port ${port}`);
 }
