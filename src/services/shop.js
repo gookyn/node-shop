6d78adc4748dd3e776fc0120de3f0b4ff60d3a21
liujinge <liujinge.home@gmail.com>
2021-08-08 17:34:22 +0800

    feat(./): 实现模型层与持久化
    新增 Sequelize ORM + sqlite3，实现模型层与持久化

--- src/services/shop.js
+++ src/services/shop.js
@@ -1,66 +1,42 @@
-// 店铺数据
-const memoryStorage = {
-  1001: { name: '良品铺子' },
-  1002: { name: '伊利' },
-  1003: { name: '三只松鼠' },
-  1004: { name: '百草味' },
-};
-
-// 模拟延时
-async function delay(ms = 200) {
-  await new Promise((r) => setTimeout(r, ms));
-}
+const { Shop } = require('../models');
 
 class ShopService {
-  async init() {
-    await delay();
-  }
+  async init() {}
 
   async find({ id, pageIndex = 0, pageSize = 10 }) {
-    await delay();
-
     if (id) {
-      return [memoryStorage[id]].filter(Boolean);
+      return [await Shop.findByPk(id)];
     }
 
-    return Object.keys(memoryStorage)
-      .slice(pageIndex * pageSize, (pageIndex + 1) * pageSize)
-      .map((id) => ({ id, ...memoryStorage[id] }));
+    return await Shop.findAll({
+      offset: pageIndex * pageSize,
+      limit: pageSize,
+    });
   }
 
   async create({ values }) {
-    await delay();
-
-    const id = String(1 + Object.keys(memoryStorage).reduce((m, id) => Math.max(m, id), -Infinity));
-
-    return {
-      id,
-      ...(memoryStorage[id] = values),
-    };
+    return await Shop.create(values);
   }
 
   async modify({ id, values }) {
-    await delay();
-
-    const target = memoryStorage[id];
+    const target = await Shop.findByPk(id);
 
     if (!target) {
       return null;
     }
 
-    return Object.assign(target, values);
+    Object.assign(target, values);
+    return await target.save();
   }
 
   async remove({ id }) {
-    await delay();
-
-    const target = memoryStorage[id];
+    const target = await Shop.findByPk(id);
 
     if (!target) {
       return false;
     }
 
-    return delete memoryStorage[id];
+    return target.destroy();
   }
 }
 
